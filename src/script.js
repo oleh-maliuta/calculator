d1d61fd3a8ce06598b1e99729ded3447fc93692d
Smaragdus <104268973+OlehMaliuta@users.noreply.github.com>
2024-03-01 23:13:23 +0200

    theme changing update
    * improved theme changing fanctionality so the current theme stores in the local storage and when you return to the page, it loads the saved theme;
    * implemented the dark theme style.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 const themeImg = document.getElementById('theme-img');
 
+const previousText = document.getElementById('previous-text');
+const currentText = document.getElementById('current-text');
+const resultText = document.getElementById('result-text');
+
 const one = document.getElementById('one');
 const two = document.getElementById('two');
 const three = document.getElementById('three');
@@ -13,14 +17,72 @@ const zero = document.getElementById('zero');
 const doubleZero = document.getElementById('double-zero');
 const dot = document.getElementById('dot');
 
-let previous = '';
-let current = '';
-let result = '';
+let previousVal = '';
+let currentVal = '0';
+let resultVal = '0';
+
+previousText.textContent = previousVal;
+currentText.textContent = currentVal;
+resultText.textContent = resultVal;
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+}
 
 themeImg.addEventListener('click', () => {
     if (document.body.classList.contains('dark')) {
         document.body.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
     } else {
         document.body.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
     }
+});
+
+one.addEventListener('click', () => {
+    
+});
+
+two.addEventListener('click', () => {
+    
+});
+
+three.addEventListener('click', () => {
+    
+});
+
+four.addEventListener('click', () => {
+    
+});
+
+five.addEventListener('click', () => {
+    
+});
+
+six.addEventListener('click', () => {
+    
+});
+
+seven.addEventListener('click', () => {
+    
+});
+
+eight.addEventListener('click', () => {
+    
+});
+
+nine.addEventListener('click', () => {
+    
+});
+
+zero.addEventListener('click', () => {
+    
+});
+
+doubleZero.addEventListener('click', () => {
+    
+});
+
+dot.addEventListener('click', () => {
+    
 });
