c60c2890dcdb43dfae148d3e352e5ab1f225188b
Smaragdus <104268973+OlehMaliuta@users.noreply.github.com>
2024-03-11 17:22:51 +0200

    js update
    * changed the purpose of the sqrt button so now it writes the power now;
    * added functionality for every button;
    * small html changes;
    * small css changes.

--- src/script.js
+++ src/script.js
@@ -4,22 +4,17 @@ const previousText = document.getElementById('previous-text');
 const currentText = document.getElementById('current-text');
 const resultText = document.getElementById('result-text');
 
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const zero = document.getElementById('zero');
-const doubleZero = document.getElementById('double-zero');
-const dot = document.getElementById('dot');
+let buttons = {};
+
+document.querySelectorAll('.option').forEach(el => {
+    buttons[el.id] = el;
+});
 
 let previousVal = '';
 let currentVal = '0';
 let resultVal = '0';
+
+let freeBracketCount = 0;
 
 previousText.textContent = previousVal;
 currentText.textContent = currentVal;
@@ -27,6 +22,113 @@ resultText.textContent = resultVal;
 
 if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.add('dark');
+}
+
+function evaluate(value, bracketCount) {
+    let brackets = '';
+
+    for (let i = 0; i < bracketCount; i++) {
+        brackets += ')';
+    }
+
+    return Function(`'use strict'; return (${value.replace('^', '**') + brackets})`)().toString()
+}
+
+function setPreviousVal(content) {
+    previousVal = content;
+    previousText.textContent = previousVal;
+}
+
+function setCurrentVal(content) {
+    currentVal = content;
+    currentText.textContent = currentVal;
+}
+
+function setResultVal(content) {
+    resultVal = content;
+    resultText.textContent = resultVal;
+}
+
+function addDigit(digit) {
+    const digitExamples = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '00'];
+    const operationExamples = ['+', '-', '*', '/', '^'];
+    const lastChar = currentVal.charAt(currentVal.length - 1);
+
+    if (!digitExamples.includes(digit)) {
+        return false;
+    }
+
+    if (
+        digit === '00' &&
+        (
+            (lastChar === '0' && !digitExamples.includes(currentVal.charAt(currentVal.length - 2))) ||
+            !digitExamples.includes(lastChar)
+        )
+    ) {
+        return false;
+    }
+
+    if (
+        digit === '0' &&
+        (lastChar === '0' && !digitExamples.includes(currentVal.charAt(currentVal.length - 2)))
+    ) {
+        return false;
+    }
+
+    if (currentVal === '0') {
+        if (digit === '0' || digit === '00') {
+            return false;
+        }
+
+        setCurrentVal(digit);
+        setResultVal(evaluate(currentVal, freeBracketCount));
+        return true;
+    }
+
+    if (
+        !digitExamples.includes(lastChar) &&
+        !operationExamples.includes(lastChar) &&
+        lastChar !== '.' &&
+        lastChar !== '('
+    ) {
+        return false;
+    }
+
+    setCurrentVal(currentVal + digit);
+    setResultVal(evaluate(currentVal, freeBracketCount));
+    return true;
+}
+
+function addOperator(operator) {
+    const operatorExamples = ['+', '-', '*', '/', '^'];
+    const digitExamples = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const lastChar = currentVal.charAt(currentVal.length - 1);
+
+    if (!operatorExamples.includes(operator)) {
+        return false;
+    }
+
+    if (operator === '-') {
+        if (currentVal === '0') {
+            setCurrentVal(operator);
+            return true;
+        }
+
+        if (lastChar === '(') {
+            setCurrentVal(currentVal + operator);
+            return true;
+        }
+    }
+
+    if (
+        !digitExamples.includes(lastChar) &&
+        lastChar !== ')'
+    ) {
+        return false;
+    }
+
+    setCurrentVal(currentVal + operator);
+    return true;
 }
 
 themeImg.addEventListener('click', () => {
@@ -39,50 +141,215 @@ themeImg.addEventListener('click', () => {
     }
 });
 
-one.addEventListener('click', () => {
-    
+buttons['one'].addEventListener('click', () => {
+    addDigit('1');
 });
 
-two.addEventListener('click', () => {
-    
+buttons['two'].addEventListener('click', () => {
+    addDigit('2');
 });
 
-three.addEventListener('click', () => {
-    
+buttons['three'].addEventListener('click', () => {
+    addDigit('3');
 });
 
-four.addEventListener('click', () => {
-    
+buttons['four'].addEventListener('click', () => {
+    addDigit('4');
 });
 
-five.addEventListener('click', () => {
-    
+buttons['five'].addEventListener('click', () => {
+    addDigit('5');
 });
 
-six.addEventListener('click', () => {
-    
+buttons['six'].addEventListener('click', () => {
+    addDigit('6');
 });
 
-seven.addEventListener('click', () => {
-    
+buttons['seven'].addEventListener('click', () => {
+    addDigit('7');
 });
 
-eight.addEventListener('click', () => {
-    
+buttons['eight'].addEventListener('click', () => {
+    addDigit('8');
 });
 
-nine.addEventListener('click', () => {
-    
+buttons['nine'].addEventListener('click', () => {
+    addDigit('9');
 });
 
-zero.addEventListener('click', () => {
-    
+buttons['zero'].addEventListener('click', () => {
+    addDigit('0');
 });
 
-doubleZero.addEventListener('click', () => {
-    
+buttons['double-zero'].addEventListener('click', () => {
+    addDigit('00');
 });
 
-dot.addEventListener('click', () => {
-    
+buttons['dot'].addEventListener('click', () => {
+    const digitExamples = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+    if (!digitExamples.includes(currentVal.charAt(currentVal.length - 1))) {
+        return;
+    }
+
+    let dotFound = false;
+
+    for (let i = currentVal.length - 1; i >= 0; i--) {
+        if (currentVal[i] === '.') {
+            dotFound = true;
+        }
+
+        if (!digitExamples.includes(currentVal[i])) {
+            break;
+        }
+    }
+
+    if (!dotFound) {
+        setCurrentVal(currentVal + '.');
+    }
+});
+
+buttons['add'].addEventListener('click', () => {
+    addOperator('+');
+});
+
+buttons['subtract'].addEventListener('click', () => {
+    addOperator('-');
+});
+
+buttons['multiply'].addEventListener('click', () => {
+    addOperator('*');
+});
+
+buttons['divide'].addEventListener('click', () => {
+    addOperator('/');
+});
+
+buttons['pow'].addEventListener('click', () => {
+    addOperator('^')
+});
+
+buttons['percent'].addEventListener('click', () => {
+    const digitExamples = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const operatorExamples = ['+', '-', '*', '/', '^'];
+    let lastChar = currentVal.charAt(currentVal.length - 1);
+
+    if (!digitExamples.includes(lastChar)) {
+        return;
+    }
+
+    let percent = '';
+    let idx = -1;
+
+    for (let i = currentVal.length - 1, j = 0; i >= 0; i--, j++) {
+        if (!digitExamples.includes(currentVal[i]) && currentVal[i] !== '.') {
+            idx = j;
+            break;
+        }
+    }
+
+    if (idx === -1) {
+        const value = String(currentVal / 100);
+        setCurrentVal(value);
+        setResultVal(value);
+        return;
+    }
+
+    percent = currentVal.slice(-idx);
+    setCurrentVal(currentVal.slice(0, -idx));
+
+    lastChar = currentVal.charAt(currentVal.length - 1);
+
+    const value = evaluate(operatorExamples.includes(lastChar) ? currentVal.slice(0, -1) : currentVal, freeBracketCount) / 100 * Number(percent);
+
+    setCurrentVal(currentVal + value);
+    setResultVal(evaluate(currentVal, freeBracketCount));
+});
+
+buttons['left-bracket'].addEventListener('click', () => {
+    const operatorExamples = ['+', '-', '*', '/', '^'];
+    const lastChar = currentVal.charAt(currentVal.length - 1);
+
+    if (
+        !operatorExamples.includes(lastChar) &&
+        currentVal !== '0'
+    ) {
+        return;
+    }
+
+    if (currentVal === '0') {
+        setCurrentVal('(');
+    }
+    else {
+        setCurrentVal(currentVal + '(');
+    }
+
+    freeBracketCount++;
+});
+
+buttons['right-bracket'].addEventListener('click', () => {
+    const digitExamples = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const lastChar = currentVal.charAt(currentVal.length - 1);
+
+    if (!digitExamples.includes(lastChar)) {
+        return;
+    }
+
+    if (freeBracketCount <= 0) {
+        return;
+    }
+
+    setCurrentVal(currentVal + ')');
+    freeBracketCount--;
+});
+
+buttons['delete'].addEventListener('click', () => {
+    const digitExamples = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const value = currentVal.slice(0, -1);
+    const lastChar = currentVal.charAt(currentVal.length - 1)
+    const lastCharOfValue = value.charAt(value.length - 1);
+
+    if (lastChar === ')') {
+        freeBracketCount++;
+    }
+
+    if (lastChar === '(') {
+        freeBracketCount--;
+    }
+
+    if (
+        digitExamples.includes(lastCharOfValue) ||
+        lastCharOfValue === ')'
+    ) {
+        setResultVal(evaluate(value, freeBracketCount));
+    }
+
+    setCurrentVal(value === '' ? '0' : value);
+
+    if (currentVal === '0') {
+        setResultVal('0');
+    }
+});
+
+buttons['c'].addEventListener('click', () => {
+    setPreviousVal('');
+    setCurrentVal('0');
+});
+
+buttons['ac'].addEventListener('click', () => {
+    setPreviousVal('');
+    setCurrentVal('0');
+    setResultVal('0');
+});
+
+buttons['equal'].addEventListener('click', () => {
+    const operatorExamples = ['+', '-', '*', '/', '^'];
+    const lastChar = currentVal.charAt(currentVal.length - 1);
+
+    if (!operatorExamples.includes(lastChar) && lastChar !== '.' && lastChar !== '(') {
+        const res = evaluate(currentVal, freeBracketCount);
+        setPreviousVal(`${currentVal}=${res}`);
+        setCurrentVal(res);
+        setResultVal(res);
+    }
 });
